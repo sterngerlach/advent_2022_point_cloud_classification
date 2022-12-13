18e35be5c6e687219cb7a7e477bffca56f86b489
Stern Gerlach <std.experimental.optional@gmail.com>
2022-12-13 22:13:16 +0900

    Add hls/src/inference_cls_naive_fail.hpp for the naive implementation of classification network

---
+++ hls/src/inference_cls_naive_fail.hpp
@@ -0,0 +1,73 @@
+
+// inference_cls_naive_fail.hpp
+
+#ifndef INFERENCE_CLS_NAIVE_FAIL_HPP
+#define INFERENCE_CLS_NAIVE_FAIL_HPP
+
+#include "batch_norm_1d.hpp"
+#include "data_transfer.hpp"
+#include "layer_types.hpp"
+#include "linear.hpp"
+#include "net_params.hpp"
+#include "vector_utils.hpp"
+
+// Naive implementation of the parameter initialization
+// `T` is the type for parameters
+template <typename T>
+void InitializeClsNaive(LinearParams<T, kClsDims0, kClsDims1>* fc1,
+                        LinearParams<T, kClsDims1, kClsDims2>* fc2,
+                        LinearParams<T, kClsDims2, kClsDims3>* fc3,
+                        BatchNorm1dParams<T, kClsDims1>* bn1,
+                        BatchNorm1dParams<T, kClsDims2>* bn2,
+                        const float* params1,
+                        const float* params2,
+                        const float* params3)
+{
+#pragma HLS INLINE off
+
+  ReadBlockParamsNaive<T, kClsDims0, kClsDims1>(fc1, bn1, params1);
+  ReadBlockParamsNaive<T, kClsDims1, kClsDims2>(fc2, bn2, params2);
+  ReadLinearParamsNaive<T, kClsDims2, kClsDims3>(fc3, params3, 0);
+}
+
+// Naive implementation of the classification network
+// `T` is the type for layer input, output, and intermediate results
+// `U` is the type for parameters
+template <typename T, typename U>
+void InferenceClsNaive(const T feature[kFeatDims5],
+                       float* out_logits,
+                       const LinearParams<U, kClsDims0, kClsDims1>* fc1,
+                       const LinearParams<U, kClsDims1, kClsDims2>* fc2,
+                       const LinearParams<U, kClsDims2, kClsDims3>* fc3,
+                       const BatchNorm1dParams<U, kClsDims1>* bn1,
+                       const BatchNorm1dParams<U, kClsDims2>* bn2)
+{
+#pragma HLS INLINE off
+
+  static_assert(kFeatDims5 == kClsDims0,
+                "Feature dimension should be equal to the input dimension");
+
+  // Input, output, and intermediate results
+  T x0[kClsDims1];
+  T x1[kClsDims1];
+  T x2[kClsDims2];
+  T x3[kClsDims2];
+  T x4[kClsDims3];
+
+  // Compute logits
+  LinearNaive<T, U, kClsDims0, kClsDims1, false>(
+    feature, x0, fc1->weight, fc1->bias);
+  BatchNorm1dReLUNaive<T, U, kClsDims1>(
+    x0, x1, bn1->scale, bn1->bias, bn1->mean);
+  LinearNaive<T, U, kClsDims1, kClsDims2, false>(
+    x1, x2, fc2->weight, fc2->bias);
+  BatchNorm1dReLUNaive<T, U, kClsDims2>(
+    x2, x3, bn2->scale, bn2->bias, bn2->mean);
+  LinearNaive<T, U, kClsDims2, kClsDims3, false>(
+    x3, x4, fc3->weight, fc3->bias);
+
+  // Write the result
+  WriteTensor1dNaive<T, kClsDims3>(out_logits, x4, 0);
+}
+
+#endif // INFERENCE_CLS_NAIVE_FAIL_HPP
